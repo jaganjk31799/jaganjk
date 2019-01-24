ddde5d2ca3902213d878831c3f99d41d44f63c96
jaganjk31799 <46890838+jaganjk31799@users.noreply.github.com>
2019-01-24 16:06:02 +0530

    Create po.c

---
+++ po.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+int main()
+{
+    int a;
+    scanf("%d",&a);
+    if(a>0)
+    {
+        printf("positive");
+    }
+    if(a<0)
+    {
+        printf("negative");
+    }
+    if(a==0)
+    {
+        printf("zero");
+ 
+   }
+   return 0;
+}
